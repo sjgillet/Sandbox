664e4833420acd99aa409055f97b37e8f523c8c1
Stuart Gillette <sjgillet@mtu.edu>
2017-12-30 18:19:29 -0500

    installed npm modules and created sample component. Angular page launches successfully and cli updates properly.

---
+++ ngApp/src/app/sampleComponent/sample.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'; 
+
+@Component ({
+    selector: 'sample-component',
+    template: `
+    <div>
+        <h1>HELLO WORLD AGAIN!</h1>
+    </div>
+    
+    `
+})
+
+export class SampleComponent {
+
+}
