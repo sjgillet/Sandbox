a7d1e73376ea41665d72a9f918b1279bd18a823f
Stuart Gillette <sjgillet@mtu.edu>
2018-01-16 11:12:59 -0500

    initial setup

---
+++ SandboxServer/src/main/java/com/sandbox/model/Castle.java
@@ -0,0 +1,5 @@
+package com.sandbox.model;
+
+public class Castle {
+
+}
--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-
-
 import { AppComponent } from './app.component';
-import { SampleComponent } from './sampleComponent/sample.component'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SampleComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
